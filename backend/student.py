d2b71716ff1f266a4a7098a9101cf1e8c3703d6f
Hunter Wilkins <hunter.wilkins2@gmail.com>
2022-02-19 11:59:34 -0500

    Added pla application

--- backend/student.py
+++ backend/student.py
@@ -1,7 +1,10 @@
+import os
 import config
 import backend.crypt
 from flask import Blueprint, render_template, abort, make_response, request, redirect, url_for, g
 from jinja2 import TemplateNotFound
+from werkzeug.utils import secure_filename
+from datetime import datetime
 
 student_page = Blueprint('student_page', __name__, template_folder='templates')
 
@@ -19,7 +22,100 @@ def student():
         return redirect(url_for('login_page.login'))
 
     name = decoded_token['first_name']
+    uid = decoded_token['uid']
 
-    print(decoded_token)
+    mycursor = config.mydb.cursor()
+    sql = f"SELECT * FROM Application app INNER JOIN User user ON app.uid = user.uid WHERE app.uid={uid}"
+    mycursor.execute(sql)
+    applicationResult = mycursor.fetchone()
 
-    return render_template('student.html', name=name)
+    labels = ('aid', 'uid', 'major', 'gpa', 'essay', 'resume', 'firstPick', 'secondPick', 'thirdPick', 'uid', 'fname', 'lname', 'email')
+    
+    application = dict()
+    if not applicationResult is None:
+        application = dict(map(lambda x,y: (x, y), labels, applicationResult))
+
+        mycursor = config.mydb.cursor()
+        sql = f"SELECT course FROM Position WHERE pid={application['firstPick']}"
+        mycursor.execute(sql)
+        firstPick = mycursor.fetchone()
+
+        application['firstPick'] = firstPick[0]
+
+        mycursor = config.mydb.cursor()
+        sql = f"SELECT course FROM Position WHERE pid={application['secondPick']}"
+        mycursor.execute(sql)
+        secondPick = mycursor.fetchone()
+
+        application['secondPick'] = secondPick[0]
+
+        mycursor = config.mydb.cursor()
+
+        sql = f"SELECT course FROM Position WHERE pid={application['thirdPick']}"
+        mycursor.execute(sql)
+        thirdPick = mycursor.fetchone()
+
+        application['thirdPick'] = thirdPick[0]
+
+        sql = f"SELECT * FROM Skill WHERE uid={uid}"
+        mycursor.execute(sql)
+        skillResult = mycursor.fetchall()
+
+        application['skills'] = list(map(lambda tup: tup[2], skillResult))
+
+    return render_template('student.html', name=name, application=application)
+
+@student_page.route('/account/application', methods=['GET', 'POST'])
+def application():
+    token = request.cookies.get('jwt')
+
+    if token is None:
+        return redirect(url_for('login_page.login'))
+    
+    decoded_token = backend.crypt.decode_jwt(token)
+    type = decoded_token['type']
+    uid = decoded_token['uid']
+
+    if not type == 'student':
+        return redirect(url_for('login_page.login'))
+
+    name = decoded_token['first_name']
+
+    mycursor = config.mydb.cursor()
+
+    mycursor.execute(f"SELECT pid, course from Position")
+
+    courses = mycursor.fetchall()
+
+    if request.method == 'POST':
+        major = request.form['major']
+        gpa = request.form['gpa']
+        essay = request.form['essay']
+        firstPick = request.form['firstPick']
+        secondPick = request.form['secondPick']
+        thirdPick = request.form['thirdPick']
+
+        file = request.files['resume']
+        filename = str(datetime.now()) + '_' + secure_filename(file.filename)
+        file.save(os.path.join('static/resumes', filename))
+
+        mycursor = config.mydb.cursor()
+        sql = "INSERT INTO Application (uid, major, gpa, essay, resume, firstPick, secondPick, thirdPick) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (uid, major, gpa, essay, filename, firstPick, secondPick, thirdPick)
+        mycursor.execute(sql, val)
+
+        config.mydb.commit()
+
+        sql = "INSERT INTO Skill (uid, skill) VALUES (%s, %s)"
+        skills = []
+        for skill in request.form.to_dict(flat=False)['skills']:
+            skills.append((uid, skill))
+
+        mycursor = config.mydb.cursor()
+        mycursor.executemany(sql, skills)
+        config.mydb.commit()
+
+        return redirect(url_for('student_page.student'))
+
+    return render_template('application.html', name=name, courses=courses)
+
